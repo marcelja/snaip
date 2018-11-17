f8ac9ed839f3015846e2dfddbab4f2a5d4992b94
Marcel Jankrift <marcel@jankrift.de>
2018-11-17 15:39:37 +0100

    color nodes

--- graph.py
+++ graph.py
@@ -10,12 +10,28 @@ for c in connections:
 
 pos = nx.circular_layout(G)
 
+# nx.draw_networkx_nodes(G, pos,
+#                        nodelist=persons,
+#                        node_color='r',
+#                        node_size=2000,
+#                        alpha=0.8)
+
 nx.draw_networkx_nodes(G, pos,
-                       nodelist=persons,
+                       nodelist=persons[0:4],
                        node_color='r',
                        node_size=2000,
                        alpha=0.8)
+nx.draw_networkx_nodes(G, pos,
+                       nodelist=persons[4:9],
+                       node_color='b',
+                       node_size=2000,
+                       alpha=0.8)
+
 nx.draw_networkx_edges(G, pos, width=1.0, alpha=0.5)
+### example edges:
+# nx.draw_networkx_edges(G, pos,
+#                        edgelist=[(4, 5), (5, 6), (6, 7), (7, 4)],
+#                        width=8, alpha=0.5, edge_color='b')
 nx.draw_networkx_labels(G, pos, font_size=13)
 
 plt.axis('off')
