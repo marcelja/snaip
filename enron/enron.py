349298ece5ea51c79585e7b83c16caf14a0f7862
Marcel Jankrift <marcel@jankrift.de>
2018-12-13 15:00:21 +0100

    read email addresses

--- enron/enron.py
+++ enron/enron.py
@@ -5,11 +5,14 @@ class Inbox():
     def __init__(self, name, directory):
         self.name = name
         self.directory = directory
-        self.folders = file_names(directory + name)
+        self.folders = file_names(self.path())
         print(self.folders)
 
     def __str__(self):
         return self.name
+
+    def path(self):
+        return self.directory + self.name
 
 
 class Enron():
@@ -20,11 +23,39 @@ class Enron():
         print('number of inboxes:', len(self.inbox_names))
         self.inboxes = []
 
+        self.connections = {}
+
     def create_inboxes(self):
         for inbox_name in self.inbox_names:
             self.inboxes.append(Inbox(inbox_name, self.directory))
             print(self.inboxes[-1])
+            self.scan_inbox(self.inboxes[-1])
             break
+
+    def scan_inbox(self, inbox):
+        for folder in inbox.folders:
+            # print(folder)
+            self.scan_folder(inbox.path() + '/' + folder)
+
+    def scan_folder(self, folder_path):
+        # print(folder_path)
+        files = file_names(folder_path)
+        # print(files)
+        for file in files:
+            print(file)
+            with open(folder_path + '/' + file, 'r') as f:
+                head = [next(f) for x in range(4)]
+                date = head[1]
+                from_address = self.parse_address(head[2])
+                to_address = self.parse_address(head[3])
+                # print(from_address)
+
+    @staticmethod
+    def parse_address(address_field):
+        # print(address_field)
+        print(address_field.strip())
+
+
 
 
 def file_names(directory):
